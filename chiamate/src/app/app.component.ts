d6d4866b6f55e5cd46947c5080d085ff631ef297
KarlaHerrera15 <162621653+KarlaHerrera15@users.noreply.github.com>
2025-05-21 17:25:55 +0000

    punto 3

--- chiamate/src/app/app.component.ts
+++ chiamate/src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Prenotazione } from './model/prenotazione.model';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'chiamate';
+  vettPrenotazioni: Prenotazione[] = [];
+  o!: Observable<Prenotazione[]>;
+  oPost! : Observable<any>;
+  postData : any;
+  loading: boolean = false;
 }
